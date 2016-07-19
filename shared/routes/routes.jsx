ec4e4359227aef6f9134111d0dcdf03eaf141efc
Aaron Rice <adrice727@gmail.com>
2016-07-18 17:23:15 -0700

    Fix import statements in routes.

--- shared/routes/routes.jsx
+++ shared/routes/routes.jsx
@@ -1,12 +1,8 @@
 import { Route } from 'react-router';
 import React from 'react';
-import App from './views/App/App';
-import ProjectSearch from './views/ProjectSearch/ProjectSearch';
-import Project from './views/Project/Project';
-
+import App from '../../shared/components/views/app';
 export default (
-  <Route handler={ App }>
-    <Route name='projectSearch' path="search" handler={ProjectSearch}/>
-    <Route name='project' path="/project/:projectId" handler={Project}/>
+  <Route handler={App}>
+
   </Route>
 );

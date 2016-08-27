0aeeafc5b1e90cee65cdc53edc51161ddb52e212
Aaron Rice <adrice727@gmail.com>
2016-08-26 17:09:05 -0700

    Udpate cors whitelist.

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,12 +1,13 @@
 const cors = require('cors');
 
 const whitelist = [
-  'http://localhost:3000'
+  'http://localhost:3000',
+  'http://localhost:3002'
 ];
 
 const options = {
   origin(origin, callback) {
-  	const isWhitelisted = whitelist.indexOf(origin) !== -1;
+    const isWhitelisted = whitelist.indexOf(origin) !== -1;
     callback(null, isWhitelisted);
   }
 };

3d1efc4ffa42e50171a36ae204ff0f6ae56febc5
Aaron Rice <adrice727@gmail.com>
2016-09-15 11:02:36 -0700

    Add logout button to header.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
+import { logoutUser } from '../../actions/currentUserActions';
+import { setInstructor } from '../../actions/instructorActions';
 import './Header.css';
 import logo from './logo.svg';
 
 
-export const Header = () => (
-  <div className="Header">
-    <span className="Header-text">OpenTok Classroom</span>
-    <img className="Header-logo" src={logo} alt="TokBox Logo" />
-  </div>
-);
+class Header extends Component {
 
-export default Header;
+  constructor(props) {
+    super(props);
+    this.onLogout = this.onLogout.bind(this);
+  }
+
+  onLogout() {
+    const { dispatch, user } = this.props;
+    if (user.role === 'instructor') {
+      dispatch(setInstructor(null))
+    }
+    dispatch(logoutUser());
+    browserHistory.push('/');
+  }
+
+  render() {
+    const { user } = this.props;
+    return (
+      <div className="Header">
+        <span className="Header-text">OpenTok Classroom</span>
+        { user ?
+          <button className="Header-logout btn transparent" onClick={this.onLogout}>Log out</button>
+          : <img className="Header-logo" src={logo} alt="TokBox Logo" />
+        }
+      </div>
+    )
+  }
+
+}
+
+const mapStateToProps = (state, { params }) => ({
+  user: state.currentUser
+});
+
+export default connect(
+  mapStateToProps
+)(Header);

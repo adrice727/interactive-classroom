7929d8ff2c36aab4dd0b2bc8a46cef71a8f11340
Aaron Rice <adrice727@gmail.com>
2016-09-22 16:49:42 -0700

    Parse response based on type.

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -8,6 +8,17 @@ const backendUrl = 'https://opentok-classroom-server.herokuapp.com';
 const url = origin === 'localhost' ? 'http://localhost:3030' : backendUrl;
 
 /** Helper methods */
+
+// Parse response based on type
+const parseResponse = response => {
+  const contentType = response.headers.get('content-type').split(';')[0];
+  if (contentType === 'application/json') {
+    return response.json();
+  } else if ( contentType === 'text/html') {
+    return response.text();
+  }
+};
+
 const request = (method, route, body) =>
   new Request(`${url}/${route}`, {
       method: R.toUpper(method),
@@ -20,7 +31,7 @@ const request = (method, route, body) =>
 const get = route =>
   new Promise((resolve, reject) => {
     fetch(request('get', route))
-    .then(response => response.json())
+    .then(response => parseResponse(response))
     .then(resolve)
     .catch(reject)
   });
@@ -28,7 +39,7 @@ const get = route =>
 const post = (route, body) =>
   new Promise((resolve, reject) => {
   fetch(request('post', route, body))
-    .then(response => response.json())
+    .then(response => parseResponse(response))
     .then(resolve)
     .catch(reject)
   });
@@ -37,7 +48,7 @@ const post = (route, body) =>
 const del = (route, body) =>
   new Promise((resolve, reject) => {
   fetch(request('delete', route, body))
-    .then(response => response.json())
+    .then(response => parseResponse(response))
     .then(resolve)
     .catch(reject)
   });

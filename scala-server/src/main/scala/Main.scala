4ee086b86e81e350484d1253c20079fe3960783b
Aaron Rice <adrice727@gmail.com>
2016-10-31 15:10:37 -0700

    Add route definitions.

--- scala-server/src/main/scala/Main.scala
+++ scala-server/src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+
 import io.finch._
 import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http.{Request, Response}
@@ -7,8 +8,14 @@ object Main extends App {
   val getHello: Endpoint[String] = get("hello") { Ok("Hello, World!") }
   val getIndex: Endpoint[String] = get(/) { Ok("Hello, Index!") }
 
+  val validateUser: Endpoint[String] = post("user") { Ok("Hello, Index!") }
+  val getClassroom: Endpoint[String] = get("classroom") { Ok("Hello, Index!") }
+  val getAllClassrooms: Endpoint[String] = get("classrooms") { Ok("Hello, Index!") }
+  val createClassroom: Endpoint[String] = post("classroom") { Ok("Hello, Index!") }
+  val removeClassroom: Endpoint[String] = delete("classroom") { Ok("Hello, Index!") }
+
   val api: Service[Request, Response] = (
-    getHello :+: getIndex
+    getHello :+: getIndex :+: validateUser :+: getClassroom :+: getAllClassrooms :+: createClassroom :+: removeClassroom
   ).toServiceAs[Text.Plain]
 
   Await.ready(Http.server.serve(":8080", api))

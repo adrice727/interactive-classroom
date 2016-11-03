22d26b307d2bd593dfde834cea8cada9d86f5c6f
Aaron Rice <adrice727@gmail.com>
2016-11-03 16:03:56 -0700

    Fix firebase imports.

--- scala-server/src/main/scala/firebase.scala
+++ scala-server/src/main/scala/firebase.scala
@@ -1,9 +1,15 @@
+import java.io.FileInputStream
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.database._
+
 object Firebase {
   val options = new FirebaseOptions.Builder()
     .setDatabaseUrl("https://opentok-classroom.firebaseio.com")
     .setServiceAccount(new FileInputStream("../config/firebaseCredentials.json"))
     .build();
   FirebaseApp.initializeApp(options)
-  val database = FirebaseDatabase.getInstance()
-  def ref : DatabaseReference = database
+  def ref(path: String): DatabaseReference = {
+    FirebaseDatabase.getInstance().getReference(path)
+  }
 }

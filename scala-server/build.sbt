80030069422eb29da3cc78339703638d2e62b880
Aaron Rice <adrice727@gmail.com>
2016-12-20 11:59:58 -0800

    Initialize OpenTok SDK.

--- scala-server/build.sbt
+++ scala-server/build.sbt
@@ -7,6 +7,8 @@ lazy val root = (project in file(".")).
       "com.github.finagle" %% "finch-core" % "0.11.0-M4",
       "com.github.finagle" %% "finch-circe" % "0.11.0-M4",
       "io.circe" %% "circe-generic" % "0.5.1",
+      "io.circe" %% "circe-parser" % "0.5.1",
+      "io.circe" %% "circe-optics" % "0.5.1",
       "com.tokbox" % "opentok-server-sdk" % "2.3.2",
       "com.google.firebase" % "firebase-server-sdk" % "3.0.1"
     )
--- scala-server/src/main/scala/OpenTok.scala
+++ scala-server/src/main/scala/OpenTok.scala
@@ -1,10 +1,33 @@
+import java.io.InputStream
+import scala.io.Source
 import com.opentok.OpenTok
-import com.opentok.Session;
-import com.opentok.SessionProperties;
+import com.opentok.Session
+import com.opentok.SessionProperties
+import com.opentok.MediaMode
+import io.circe._, io.circe.parser._, io.circe.generic.semiauto._, io.circe.parser.decode
+import cats.syntax.either._
+
+
+case class OpentokCredentials(apiKey: Int, apiSecret: String)
 
 object Opentok {
+  /** Initialize */
+  val credentialsJSON: String = {
+    val stream: InputStream = getClass.getResourceAsStream("/opentokCredentials.json")
+    scala.io.Source.fromInputStream(stream).mkString
+  }
+  val doc: Json = parse(credentialsJSON).getOrElse(Json.Null)
+  implicit val credentialsDecoder: Decoder[OpentokCredentials] = deriveDecoder[OpentokCredentials]
+  val credentials: OpentokCredentials = credentialsDecoder(doc.hcursor).getOrElse(null)
+  val opentok: OpenTok = new OpenTok(credentials.apiKey, credentials.apiSecret)
 
+  def createSession = {
+    val session: Session = opentok.createSession(new SessionProperties.Builder()
+      .mediaMode(MediaMode.ROUTED)
+      .build());
+  }
 }
+
 
 
 

0bf83015be3ba6f8d1a98df2ca26665c41ae2f3b
Aaron Rice <adrice727@gmail.com>
2017-01-04 11:29:33 -0800

    Make values inside Firebase object private.

--- server/src/main/scala/Firebase.scala
+++ server/src/main/scala/Firebase.scala
@@ -6,14 +6,12 @@ import com.google.firebase.database._
 case class FirebaseException(s: String) extends Exception(s)
 
 object Firebase {
-  val credentials : InputStream = getClass.getResourceAsStream("/firebaseCredentials.json")
-  val options = new FirebaseOptions.Builder()
+  private val credentials : InputStream = getClass.getResourceAsStream("/firebaseCredentials.json")
+  private val options = new FirebaseOptions.Builder()
     .setDatabaseUrl("https://opentok-classroom.firebaseio.com")
     .setServiceAccount(credentials)
     .build();
   FirebaseApp.initializeApp(options)
-  def ref(path: String): DatabaseReference = {
-    val database = FirebaseDatabase.getInstance()
-    database.getReference(path)
-  }
+  private val database = FirebaseDatabase.getInstance()
+  def ref(path: String): DatabaseReference = database.getReference(path)
 }
